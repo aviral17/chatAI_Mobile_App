2f12ba315d737dfc117f77a010810e9504a869a5
Aviral <36530173+aviral17@users.noreply.github.com>
2025-04-15 21:36:16 +0530

    updated SocialProviders.tsx

--- src/components/SocialProviders.tsx
+++ src/components/SocialProviders.tsx
@@ -1,3 +1,5 @@
+// update auth0 as per latest requirements
+
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
